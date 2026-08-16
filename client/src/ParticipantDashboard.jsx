650e597d8580bc459647bf81775e4e871ff5b7ce
abdurrahmanjihad177-dev <abdurrahmanjihad177@gmail.com>
2026-08-16 20:19:05 +0600

    Connect frontend to live server

--- client/src/ParticipantDashboard.jsx
+++ client/src/ParticipantDashboard.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import players from "./players";
 import "./ParticipantDashboard.css";
 
-const socket = io("http://localhost:5000");
+const socket =  io("https://friends-football-auction.onrender.com")
 
 const AUCTION_PHASES = [
   "Attackers",
